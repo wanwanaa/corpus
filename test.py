144afa6d9538ea7f80d342a9cba0e730dc8af326
wanwanaa <1551612415@qq.com>
2018-08-26 11:26:22 +0800

    second commit

---
+++ test.py
@@ -0,0 +1,18 @@
+import numpy as np
+import pickle
+f = np.load('data/test.npz')
+a = f['index']
+print(a[0])
+print(a[1])
+print(a[2])
+with open('data/word2index.pkl', 'rb') as f:
+    a = pickle.load(f)
+    print(a)
+
+
+'''a = [1, 2, 3]
+pickle_file = open('a.pkl', 'wb')
+pickle.dump(a, pickle_file)
+with open('a.pkl', 'rb') as f:
+    a = pickle.load(f)
+print(a[1])'''
